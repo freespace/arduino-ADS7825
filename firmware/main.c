9354b64c3013bf473a0a1c197eb39fc0e3332ae3
Shuning Bian <freespace@gmail.com>
2013-09-09 17:15:21 +0100

    Implements use of timer/counter0 for triggering and trigger division in main.c

--- firmware/main.c
+++ firmware/main.c
@@ -2,24 +2,77 @@
 
 #include <avr/io.h>
 #include <avr/sfr_defs.h>
+#include <avr/interrupt.h>
+
 #include "uart.h"
 
 #include "ads7825.h"
 
+#define BUFFER_SIZE               (512)
+
+void timer_init(uint8_t top) {
+  // setup counter0 issue an interrupt every time its value reaches top
+
+  // OC0A and OC0B disconnected, normal waveform generation mode
+  TCCR0A = 0x00 | _BV(WGM01);
+
+  // CTC waveform generation mode, clearing the timer on match
+  TCCR0B = 0x00 | _BV(CS02) | _BV(CS01);
+
+  // set the value at which we roll over, using OCR0A
+  OCR0A = top;
+
+  // issue an interrupt when OCR0A matches TCNT0
+  TIMSK0 = 0x00 | _BV(OCIE0A);
+
+  TCNT0 = 0;
+
+  sei();
+}
+
+
+typedef struct {
+  int16_t data[BUFFER_SIZE];
+  uint16_t writepos;
+} Buffer;
+
+volatile Buffer buffer;
+
+ISR(TIMER0_COMPA_vect) {
+  if (buffer.writepos < BUFFER_SIZE) {
+    buffer.data[buffer.writepos++] = adc_read_analog(1);
+    buffer.data[buffer.writepos++] = adc_read_analog(2);
+    buffer.data[buffer.writepos++] = adc_read_analog(3);
+    buffer.data[buffer.writepos++] = adc_read_analog(0);
+  }
+}
+
 int main(void) {
   uart_init();
   adc_init();
+  timer_init(10);
   
   printf("Ready\n");
-  adc_read_analog(0);
-  printf("Primed\n");
 
   for (;;) {
-    printf("%6d %6d %6d %6d\n",
-        adc_read_analog(1),
-        adc_read_analog(2),
-        adc_read_analog(3),
-        adc_read_analog(0));
+    int c = getchar();
+    if (c == 'r') {
+      printf("%6d, %6d, %6d, %6d\n",
+          adc_read_analog(1),
+          adc_read_analog(2),
+          adc_read_analog(3),
+          adc_read_analog(0));
+    } else if (c == 'p') {
+      int idx;
+      for (idx = 0; idx < buffer.writepos; idx += 4) {
+      printf("%d %d %d %d\n",
+          buffer.data[idx+0],
+          buffer.data[idx+1],
+          buffer.data[idx+2],
+          buffer.data[idx+3]);
+      }
+      printf("END\n");
+    }
   }
 
   return 0;
