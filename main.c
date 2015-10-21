b375dab79bf66a31e265e3edf3cae7e2da580943
Shuning Bian <freespace@gmail.com>
2015-10-21 18:01:44 +0100

    Implements new command B in main.c

--- main.c
+++ main.c
@@ -79,6 +79,137 @@ void _err(uint8_t code) {
   printf("ERR%d\n", code);
 }
 
+void _readserial(void) {
+  int c, a, r, n;
+
+  // enable interrupts while waiting for serial
+  sei();
+
+  c = getchar();
+
+  // interrupts are enabled only for o and f for ease of debugging
+  switch(c) {
+    case 'o':
+    case 'f':
+      break;
+    default:
+      cli();
+  }
+
+  if (c == 'r') {
+    // prime channel 0 for read, just in case, even though we should always
+    // end with channel 0 primed for next read
+    adc_read_analog(0);
+
+    // we don't do this inside a printf statement because we need the reads
+    // to occur in a specific order, and function argument evaluation order
+    // is not guaranteed
+    int chan0 = adc_read_analog(1);
+    int chan1 = adc_read_analog(2);
+    int chan2 = adc_read_analog(3);
+    int chan3 = adc_read_analog(0);
+
+    printf("%6d, %6d, %6d, %6d\n",
+        chan0,
+        chan1,
+        chan2,
+        chan3);
+  } else if (c == 's') {
+    // a byte is expected after this command that has value 1..4 in ASCII.
+    a = getchar();
+    nchannels = a - '0';
+    if (nchannels > CHANNELS_AVAILABLE || nchannels <= 0) {
+      nchannels = CHANNELS_AVAILABLE;
+      _err(CHANNEL_OUT_OF_RANGE_ERROR);
+    } else {
+      // prime channel 0 for read, just in case, even though we should always
+      // end with channel 0 primed for next read
+      adc_read_analog(0);
+
+      // reset to start of the buffer
+      buffer.writepos = 0;
+
+      // reset the trigger
+      trigger_reset();
+
+      // this provides feedback of when we have started triggering
+      PORTB = 0;
+
+      _ack();
+    }
+  } else if (c == 'p') {
+    //printf("TCNT0=%d OCR0A=%d\n", TCNT0, OCR0A);
+    int idx, ch;
+    // writepos should never be greater than BUFFER_SIZE, but lets make sure
+    if (buffer.writepos > BUFFER_SIZE) buffer.writepos = BUFFER_SIZE;
+
+    for (idx = 0; idx+nchannels-1 < buffer.writepos; idx += nchannels) {
+      printf("%d ", idx/nchannels);
+      for (ch = 0; ch < nchannels; ++ch) {
+        printf("%d ", buffer.data[idx+ch]);
+      }
+      printf("\n");
+    }
+    printf("END\n");
+  } else if (c == 'b') {
+    // sends the buffer in binary. The first 2 bytes indicate the number of
+    // bytes to follow. All data are in fact 16bit integers, sent LSB first
+    // since AVRs are little endian machines
+    fwrite((void *)&buffer.writepos, sizeof buffer.writepos, 1, stdout);
+    fwrite((void *)buffer.data, sizeof buffer.data[0], buffer.writepos, stdout);
+  } else if (c == 'c') {
+    // send the number of readings in the buffer. Each reading is from a
+    // channel, and is a 16bit integer
+    fwrite((void *)&buffer.writepos, sizeof buffer.writepos, 1, stdout);
+  } else if (c == 'o' || c == 'f') {
+    // this turns on/off a pin, another byte is expected which specified the
+    // pin to turn on. Exactly which pin is turned on depends on
+    // digital_out.
+    a = getchar();
+    a = a - '0';
+    r = digital_out_write(a, c == 'o');
+    if (r == NO_ERROR) _ack();
+    else _err(r);
+  } else if (c == 't') {
+    a = getchar();
+    a = a - '0';
+    if (a < 1 || a > 9) _err(ARGUMENT_OUT_OF_RANGE_ERROR);
+    else {
+      trigger_set(a);
+      _ack();
+    }
+  } else if (c == 'x') {
+    a = getchar();
+    a = a - '0';
+    if (a < 1 || a > 255) _err(ARGUMENT_OUT_OF_RANGE_ERROR);
+    else {
+      adc_set_exposures(a);
+      _ack();
+    }
+  } else if (c == 'B') {
+    // get the least significant byte and convert it from our adhoc
+    // base64 scheme to a value between 0..63
+    a = getchar();
+    n = a - '0';
+
+    // get the second significant byte
+    a = getchar();
+    a = (a - '0');
+
+    // calculated the original value, keeping in mind that we are working in
+    // base 64
+    n = a*64 + n;
+
+    // clamp the maximum value to the number of samples in the buffer
+    if (n > buffer.writepos) n = buffer.writepos;
+
+    // write out the number of bytes we are sending
+    fwrite((void *)&n, sizeof n, 1, stdout);
+    // then write out the bytes
+    fwrite((void *)buffer.data, sizeof buffer.data[0], n, stdout);
+  } else _err(UNKNOWN_COMMAND_ERROR);
+}
+
 int main(void) {
   uart_init();
   adc_init();
@@ -92,114 +223,8 @@ int main(void) {
 
   DDRB = _BV(PB7);
 
-  int c, a, r;
   for (;;) {
-    // enable interrupts while waiting for serial
-    sei();
-
-    c = getchar();
-
-    // interrupts are enabled only for o and f for ease of debugging
-    switch(c) {
-      case 'o':
-      case 'f':
-        break;
-      default:
-        cli();
-    }
-
-    if (c == 'r') {
-      // prime channel 0 for read, just in case, even though we should always
-      // end with channel 0 primed for next read
-      adc_read_analog(0);
-
-      // we don't do this inside a printf statement because we need the reads
-      // to occur in a specific order, and function argument evaluation order
-      // is not guaranteed
-      int chan0 = adc_read_analog(1);
-      int chan1 = adc_read_analog(2);
-      int chan2 = adc_read_analog(3);
-      int chan3 = adc_read_analog(0);
-
-      printf("%6d, %6d, %6d, %6d\n",
-          chan0,
-          chan1,
-          chan2,
-          chan3);
-    } else if (c == 's') {
-      // a byte is expected after this command that has value 1..4 in ASCII.
-      a = getchar();
-      nchannels = a - '0';
-      if (nchannels > CHANNELS_AVAILABLE || nchannels <= 0) {
-        nchannels = CHANNELS_AVAILABLE;
-        _err(CHANNEL_OUT_OF_RANGE_ERROR);
-      } else {
-        // prime channel 0 for read, just in case, even though we should always
-        // end with channel 0 primed for next read
-        adc_read_analog(0);
-
-        // reset to start of the buffer
-        buffer.writepos = 0;
-
-        // reset the trigger
-        trigger_reset();
-
-        // this provides feedback of when we have started triggering
-        PORTB = 0;
-
-        _ack();
-      }
-    } else if (c == 'p') {
-      //printf("TCNT0=%d OCR0A=%d\n", TCNT0, OCR0A);
-      int idx, ch;
-      // writepos should never be greater than BUFFER_SIZE, but lets make sure
-      if (buffer.writepos > BUFFER_SIZE) buffer.writepos = BUFFER_SIZE;
-
-      for (idx = 0; idx+nchannels-1 < buffer.writepos; idx += nchannels) {
-        printf("%d ", idx/nchannels);
-        for (ch = 0; ch < nchannels; ++ch) {
-          printf("%d ", buffer.data[idx+ch]);
-        }
-        printf("\n");
-      }
-      printf("END\n");
-    } else if (c == 'b') {
-      // sends the buffer in binary. The first 2 bytes indicate the number of
-      // bytes to follow. All data are in fact 16bit integers, sent LSB first
-      // since AVRs are little endian machines
-      fwrite((void *)&buffer.writepos, sizeof buffer.writepos, 1, stdout);
-      fwrite((void *)buffer.data, sizeof buffer.data[0], buffer.writepos, stdout);
-    } else if (c == 'c') {
-      // send the number of readings in the buffer. Each reading is from a
-      // channel, and is a 16bit integer
-      fwrite((void *)&buffer.writepos, sizeof buffer.writepos, 1, stdout);
-    } else if (c == 'o' || c == 'f') {
-      // this turns on/off a pin, another byte is expected which specified the
-      // pin to turn on. Exactly which pin is turned on depends on
-      // digital_out.
-      a = getchar();
-      a = a - '0';
-      r = digital_out_write(a, c == 'o');
-      if (r == NO_ERROR) _ack();
-      else _err(r);
-    } else if (c == 't') {
-      a = getchar();
-      a = a - '0';
-      if (a < 1 || a > 9) _err(ARGUMENT_OUT_OF_RANGE_ERROR);
-      else {
-        trigger_set(a);
-        _ack();
-      }
-    } else if (c == 'x') {
-      a = getchar();
-      a = a - '0';
-      if (a < 1 || a > 255) _err(ARGUMENT_OUT_OF_RANGE_ERROR);
-      else {
-        adc_set_exposures(a);
-        _ack();
-      }
-    } else _err(UNKNOWN_COMMAND_ERROR);
-
+    _readserial();
   }
 
   return 0;
